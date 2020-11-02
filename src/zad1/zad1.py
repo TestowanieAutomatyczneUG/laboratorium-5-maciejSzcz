cfe3e9bb3fdb345e9fe3b3577d699788e328bb8c
Maciej Szczesny <yetek.lol@gmail.com>
2020-11-02 16:02:18 +0100

    single leter evaluation passes tests

--- src/zad1/zad1.py
+++ src/zad1/zad1.py
@@ -2,3 +2,7 @@ class Hamming:
     def distance(first, second):
         if first == "" or second == "":
             return 0
+        elif len(first) == 1 and first == second:
+            return 0
+        elif len(first) == 1 and len(second) == 1:
+            return 1
